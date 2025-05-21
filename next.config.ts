004bcdee34ee3f49d344d40f22903f4e2c6e6211
Ranjula001 <ranjulailukpitiya@gmail.com>
2025-05-21 13:16:21 +0530

    gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, // Required for GitHub Pages to handle routes correctly
 };
 
 export default nextConfig;
