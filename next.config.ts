3d00e20e275a4f76667f72f02e478213f483844c
Ranjula001 <ranjulailukpitiya@gmail.com>
2025-05-21 15:21:58 +0530

    output : export

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true, // Required for GitHub Pages to handle routes correctly
 };
 
 export default nextConfig;
