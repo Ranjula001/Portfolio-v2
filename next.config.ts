0fefd5c9e9dbd1d5ee722f19cca01f834594400b
Ranjula001 <ranjulailukpitiya@gmail.com>
2025-05-21 15:40:23 +0530

    error fixes and speedCheck

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // disables Next.js image optimization API
+  },
 };
 
 export default nextConfig;
